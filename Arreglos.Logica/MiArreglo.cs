6a6b408405a2e612b3dad64b14eb0f0dd234c6f3
DairaLazcano <25030138@itesa.edu.mx>
2026-09-17 14:49:23 -0600

    Versión_02: Metodo llenar.

---
+++ Arreglos.Logica/MiArreglo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+        //Atributos
+        private int _tope;
+        private int[] _arreglo;
+
+        //Constructor
+        public MiArreglo(int n)   //ctor + tab
+        {
+            N=n;
+            _arreglo = new int[N];
+            _tope = 0;             //esta limpio el arreglo
+            
+        }
+
+        //Propiedades
+        public int N { get; }  //sin setter para no modificar el tamaño
+        public bool EstaLleno => _tope == N;
+        public bool EstaVacio => _tope == 0;
+
+
+        //Metodos
+        public void Llenar(int minimo, int maximo)
+        {
+            Random oRandom = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                _arreglo[i] = oRandom.Next(minimo, maximo + 1);
+            }
+            _tope = N;
+        }
+
+        //Metodo ToString
+        public override string ToString()
+        {
+            if (EstaVacio)
+            {
+                return "Ésta vacio";
+            }
+
+            string cadena = string.Empty;  //Limpiar una cadena de forma eficiente
+            int cont = 0;
+
+            for (int i = 0; i < _tope; i++)  //El tope apunta a la sig direccion vacia (se pone -1)
+            {
+                //cadena = cadena + _arreglo[i];
+                cadena += $"{_arreglo[i]}\t";   //interpolacion
+
+                cont++;
+
+                if(cont > 9)
+                {
+                    cont = 0;         //reinicio el contador
+                    cadena += "\n";   //salto de linea cada 10 elementos
+                }
+
+            }
+            return cadena;
+        }
+    }
+}
---
+++ Arreglos.UI.Consola/Program.cs
@@ -0,0 +1,13 @@
+﻿using Arreglos.Logica;
+
+Console.WriteLine("Operaciones de pila\n");
+
+Console.WriteLine("Arreglo");
+MiArreglo oMiArreglo = new MiArreglo(100);  //logico
+
+oMiArreglo.Llenar(1, 20);   //fisico
+
+Console.WriteLine(oMiArreglo);  //para recorrer
+
+
+Console.ReadKey();
